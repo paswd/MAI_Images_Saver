478b93716b558e1e08ae02d50798f98a0b838087
paswd <paswd@yandex.ru>
2016-03-01 17:52:10 +0300

    Test

--- imgsaver.py
+++ imgsaver.py
@@ -1,6 +1,8 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
+#Здесь скачивается изображение по заданной ссылке
+
 import urllib
 
 img = "http://www.fortrainz.ru/load/objects/tpt-pak-budok/logo.jpg"
