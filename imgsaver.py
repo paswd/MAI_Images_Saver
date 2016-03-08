dd7e3af350023086bd364834fd495f4d17cf1225
paswd <paswd@pwd-lnx>
2016-03-08 19:05:21 +0300

    Updated

--- imgsaver.py
+++ imgsaver.py
@@ -1,42 +1,56 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
 import sys
 
-alphabet = [['0', '1', '2', '3'],
-['4', '5', '6', '7'],
-['8', '9', '_', '-'],
-['A', 'B', 'C', 'D'],
-['E', 'F', 'G', 'H'],
-['I', 'G', 'K', 'L'],
-['M', 'N', 'O', 'P'],
-['Q', 'R', 'S', 'T'],
-['U', 'V', 'W', 'X'],
-['Y', 'Z', 'a', 'b'],
-['c', 'd', 'e', 'f'],
-['g', 'h', 'i', 'j'],
-['k', 'l', 'm', 'n'],
-['o', 'p', 'q', 'r'],
-['s', 't', 'u', 'v'],
-['w', 'x', 'y', 'z']]
+def PrintLink(bas, part1, part2, part3):
+	out = ""
+	for i in el1:
+		out += alphabet[el1[i]]
+
+	out += "/"
+
+	for i in el2:
+		out += alphabet[el2[i]]
+
+	out += alphabet[el3]
+	out += ".jpg"
+	print(bas + out)
+
+
+
+alphabet = ['0', '1', '2', '3',
+'4', '5', '6', '7',
+'8', '9', '_', '-',
+'A', 'B', 'C', 'D',
+'E', 'F', 'G', 'H',
+'I', 'G', 'K', 'L',
+'M', 'N', 'O', 'P',
+'Q', 'R', 'S', 'T',
+'U', 'V', 'W', 'X',
+'Y', 'Z', 'a', 'b',
+'c', 'd', 'e', 'f',
+'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n',
+'o', 'p', 'q', 'r',
+'s', 't', 'u', 'v',
+'w', 'x', 'y', 'z']
 
 base = 64
 
-def GetElementByNum(num):
-	
-
-#print(alphabet[1][1])
-
 print("Input any image VK link")
 href = sys.stdin.readline()
-#print('\n')
-el1 = [0, 0]
-el2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-el3 = 0
-strr = "abcdef"
-print(GetElementByNum())
+basic = href[:39]
 
-#while el1 != "zz":
-#	while el2 != "zzzzzzzzzzz"
-#		while el3 != "w"
+el1 = [0, 0]
+el2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+el3 = 0
+
+PrintLink(basic, el1, el2, el3)
+
+
+#print(href[:39])
+#while el1[0] < base:
+#	while el2[0] < base:
+#		while el3 < base:
--- loadimg.py
+++ loadimg.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 #Здесь скачивается изображение по заданной ссылке
